cc66f5d61d527d7c93a40258d32dac10a55678f1
0xSj <saadeljarida@gmail.com>
2026-08-14 15:12:46 +0100

    Inception main directory

---
+++ Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+	cat > /tmp/init.sql <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+EOF
+
+	exec mariadbd --user=mysql --init-file=/tmp/init.sql
+fi
+
+exec mariadbd --user=mysql
+
